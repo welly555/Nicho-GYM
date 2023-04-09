e827a34d1d73c1bf1fa61a92a007360f43f743f4
welly555 <wellyngton.targino@aluno.uepb.edu.br>
2023-04-09 18:31:33 -0300

    template login and register

--- project/gym/views.py
+++ project/gym/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def login(request):
-    return render(request, 'gym/login.html')
+    return render(request, 'gym/pages/login.html')
 
 def cadastro(request):
-    return render(request, 'gym/cadastro.html')
+    return render(request, 'gym/pages/cadastro.html')
