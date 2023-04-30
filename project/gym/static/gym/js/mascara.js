1362f2b514b10dc49cc5981dfb3dbca96b900c7a
Welly <84642588+welly555@users.noreply.github.com>
2023-04-30 13:57:15 -0300

    Hotfix/bugs (#10)
    * start forms
    
    * start forms
    
    * asdv (#6)
    
    * Remove /static from gitignore (#4)
    
    * Feature/add gym form (#5)
    
    * start forms
    
    * start forms
    
    * forms 50% check
    
    * finishe cadastro
    
    * Finish login
    
    * mascara nos campos
    
    * correção css
    
    * reparos na logica
    
    * .

---
+++ project/gym/static/gym/js/mascara.js
@@ -0,0 +1,17 @@
+var SPMaskBehavior = function (val) {
+    return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+  },
+  spOptions = {
+    onKeyPress: function(val, e, field, options) {
+        field.mask(SPMaskBehavior.apply({}, arguments), options);
+      }
+  };
+  
+  
+
+$(function(){
+    
+    $('.mask-cnpj').mask('00.000.000/0000-00', {reverse: true});
+    $('.mask-telefone').mask(SPMaskBehavior, spOptions);
+});
+
--- project/project/urls.py
+++ project/project/urls.py
@@ -18,9 +18,9 @@ from django.contrib import admin
 from django.urls import include, path
 
 
-name = 'gym'
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('gym.urls')),
+
+]
 
