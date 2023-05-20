ae606b172e6347485a13dbb8e2efb41bcc63aaae
Welly <84642588+welly555@users.noreply.github.com>
2023-05-20 17:21:58 -0300

    Develop (#24)
    * asdv (#6)
    
    * Remove /static from gitignore (#4)
    
    * Feature/add gym form (#5)
    
    * start forms
    
    * start forms
    
    * Feature/add gym form (#8)
    
    * start forms
    
    * start forms
    
    * forms 50% check
    
    * finishe cadastro
    
    * Finish login
    
    * funcionalidade de recuperação de senha
    
    * funcionalidade de recuperação de senha (#11)
    
    * Feacture/tests (#13)
    
    * funcionalidade de recuperação de senha
    
    * tests
    
    * .
    
    * Finish
    
    * Homepage (#15)
    
    * Create django tests
    
    adicionei
    
    * Update django.yml
    
    * Update django.yml
    
    * Update django.yml
    
    * Finish
    
    ---------
    
    Co-authored-by: Welly <84642588+welly555@users.noreply.github.com>
    
    * RemoveButton
    
    * StyleButtonHeader
    
    * button
    
    * Update django.yml
    
    * Update django.yml
    
    * Update django.yml
    
    * Update django.yml
    
    * Update django.yml
    
    * Update django.yml
    
    * Não sei o que fiz aqui
    
    * envio de email com o link para recuperar senha
    
    * Funcionando recuperação de senha
    
    * atualização do requereme (#23)
    
    * Feacture/cadastro alunos (#22)
    
    * Develop (#17)
    
    * asdv (#6)
    
    * Remove /static from gitignore (#4)
    
    * Feature/add gym form (#5)
    
    * start forms
    
    * start forms
    
    * Feature/add gym form (#8)
    
    * start forms
    
    * start forms
    
    * forms 50% check
    
    * finishe cadastro
    
    * Finish login
    
    * funcionalidade de recuperação de senha (#11)
    
    * Feacture/tests (#13)
    
    * funcionalidade de recuperação de senha
    
    * tests
    
    * .
    
    * Homepage (#15)
    
    * Create django tests
    
    adicionei
    
    * Update django.yml
    
    * Update django.yml
    
    * Update django.yml
    
    * Finish
    
    ---------
    
    Co-authored-by: Welly <84642588+welly555@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Weslley Almeida <112653472+AlmeidaWeslley@users.noreply.github.com>
    
    * Update README.md
    
    * cadastro_aluno
    
    * teste
    
    ---------
    
    Co-authored-by: Welly <84642588+welly555@users.noreply.github.com>
    
    * alterações
    
    ---------
    
    Co-authored-by: Weslley Almeida <weslley.almeida.f@gmail.com>
    Co-authored-by: Weslley Almeida <112653472+AlmeidaWeslley@users.noreply.github.com>

--- project/gym/forms/__init__.py
+++ project/gym/forms/__init__.py
@@ -3,3 +3,11 @@
 from .login import LoginForm
 from .register_form import RegisterForm
 from .reset_form import Recuperar_senha
+
+
+from .valid_email_form import Valid_Email
+
+
+
+from .aluno import AlunoRegister
+
